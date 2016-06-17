bac203a3dbbce720efab034d2af74f580065b55d
Marco F. Cavaliere <marco.f.cavaliere@gmail.com>
2016-06-17 17:39:57 +0200

    Timeout Increased

--- config/env/production.js
+++ config/env/production.js
@@ -21,7 +21,7 @@ module.exports = {
   //   connection: 'someMysqlServer'
   // },
   connection: 'Mongolabs_heroku',
-  hookTimeout: 60000,
+  hookTimeout: 30000,
   /***************************************************************************
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
