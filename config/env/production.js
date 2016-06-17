999adb496d99fa4db1e2bc4da9492189cfc23293
Marco F. Cavaliere <marco.f.cavaliere@gmail.com>
2016-06-17 16:54:36 +0200

    Add MongoDb on Heroku

--- config/env/production.js
+++ config/env/production.js
@@ -21,7 +21,7 @@ module.exports = {
   //   connection: 'someMysqlServer'
   // },
   connection: 'Mongolabs_heroku',
-  hookTimeout: 60000,
+  hookTimeout: 50000,
   /***************************************************************************
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
